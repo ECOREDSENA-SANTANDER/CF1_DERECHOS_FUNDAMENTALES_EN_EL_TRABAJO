34040e4363bcf93d281b6555e52cf90dd7a51ba7
PABG1990 <pedro19908454@gmail.com>
2025-08-01 14:29:14 -0500

    Act_video_i

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -21,8 +21,8 @@
           .col-lg-12
             figure
               .video
-                iframe(width="560" height="315" src="https://www.youtube.com/embed/vdPrCjWJSHo?si=X4NxENRo3LLXAua_" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen)
-            figcaption Video. 
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/NL83a4WqQis?si=BiSAkBqrirrfe_Ln" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            figcaption Video. Derechos fundamentales del trabajo
 
 </template>
 
